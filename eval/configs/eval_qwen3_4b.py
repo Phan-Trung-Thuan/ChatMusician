48eb6d0228012cdc7bffe8fb879dfe1e761a680c
Phan Trung Thuận <thuantk000@gmail.com>
2025-10-15 12:07:03 +0700

    update

--- eval/configs/eval_qwen3_4b.py
+++ eval/configs/eval_qwen3_4b.py
@@ -1,7 +1,34 @@
 from mmengine.config import read_base
+from opencompass.runners import LocalRunner
+from opencompass.tasks import OpenICLInferTask, OpenICLEvalTask
+from opencompass.partitioners import NaivePartitioner
 
 with read_base():
-    from .datasets.collections.base_medium_llama import piqa_datasets, siqa_datasets
+    from .datasets.collections.base_medium_llama import mmlu_datasets
+    from .datasets.music_theory_bench.music_theory_bench_ppl_zero_shot import music_theory_bench_datasets_zero_shot
+    from .datasets.music_theory_bench.music_theory_bench_ppl_few_shot import music_theory_bench_datasets_few_shot
     from .models.qwen.qwen3_4b import models
 
-datasets = [*piqa_datasets, *siqa_datasets]
+datasets = [
+    *mmlu_datasets,
+    *music_theory_bench_datasets_zero_shot,
+    *music_theory_bench_datasets_few_shot
+]
+
+infer = dict(
+    partitioner=dict(type=NaivePartitioner),
+    runner=dict(
+        type=LocalRunner,
+        max_num_workers=8,
+        task=dict(type=OpenICLInferTask)
+    ),
+)
+
+eval = dict(
+    partitioner=dict(type=NaivePartitioner),
+    runner=dict(
+        type=LocalRunner,
+        max_num_workers=64,
+        task=dict(type=OpenICLEvalTask)
+    ),
+)
