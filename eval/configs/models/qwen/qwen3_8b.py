d1dbe825600bf326a06ebf15c82f9a2146f29c1c
Phan Trung Thuận <thuantk000@gmail.com>
2025-10-16 17:55:16 +0700

    update

--- eval/configs/models/qwen/qwen3_8b.py
+++ eval/configs/models/qwen/qwen3_8b.py
@@ -13,11 +13,11 @@ models = [
         tokenizer_kwargs=dict(padding_side='left', truncation_side='left'),
         model_kwargs=dict(
             device_map="auto",
-            torch_dtype="auto",
+            torch_dtype="torch.bfloat16",
         ),
         max_out_len=100,
         max_seq_len=2048,
         batch_size=16,
-        run_cfg=dict(num_gpus=1, num_procs=1),
+        run_cfg=dict(num_gpus=2, num_procs=2),
     ),
 ]
