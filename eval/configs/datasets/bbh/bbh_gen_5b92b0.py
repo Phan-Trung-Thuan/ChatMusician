2199dd1f5b6d27e601e4f338b6a009cb3a209866
Phan Trung Thuận <thuantk000@gmail.com>
2025-10-16 16:23:21 +0700

    update

--- eval/configs/datasets/bbh/bbh_gen_5b92b0.py
+++ eval/configs/datasets/bbh/bbh_gen_5b92b0.py
@@ -41,8 +41,8 @@ bbh_free_form_sets = [
 
 bbh_datasets = []
 for _name in bbh_multiple_choice_sets:
-    print(__file__)
-    with open(os.path.join.build()(os.path.dirname(__file__), 'lib_prompt', f'{_name}.txt'), 'r') as f:
+    print(os.path.join(os.path.dirname(__file__), 'lib_prompt', f'{_name}.txt'))
+    with open(os.path.join(os.path.dirname(__file__), 'lib_prompt', f'{_name}.txt'), 'r') as f:
         _hint = f.read()
     bbh_infer_cfg = dict(
         prompt_template=dict(
@@ -73,7 +73,7 @@ for _name in bbh_multiple_choice_sets:
             eval_cfg=bbh_eval_cfg.copy()))
 
 for _name in bbh_free_form_sets:
-    with open(os.path.join.build()(os.path.dirname(__file__), 'lib_prompt', f'{_name}.txt'), 'r') as f:
+    with open(os.path.join(os.path.dirname(__file__), 'lib_prompt', f'{_name}.txt'), 'r') as f:
         _hint = f.read()
     bbh_infer_cfg = dict(
         prompt_template=dict(
